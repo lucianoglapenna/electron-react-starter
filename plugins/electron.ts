e423a4914dc7d2f865dd53fdf319176909d5b63f
Luciano Lapenna <lucianoglapenna@gmail.com>
2022-12-31 18:59:07 -0300

    feat(electron): improve electron vite plugin

--- plugins/electron.ts
+++ plugins/electron.ts
@@ -4,14 +4,23 @@ import { Plugin } from 'vite';
  * Configures app to work alongside Electron
  * @returns Vite `Plugin`
  */
-export default function electron(): Plugin {
-  return {
-    name: 'vite-plugin-electron-react',
-    apply: 'build', // only run this plugin when building the app
-    config(c) {
-      // alter the vite config
-      c.base = './';
-      c.build.outDir = 'build/renderer';
+export default function electron(): Plugin[] {
+  return [
+    {
+      name: 'vite-plugin-electron-react',
+      apply: 'serve', // only run this plugin when serving the app,
+      config(c) {
+        c.clearScreen = false;
+      },
     },
-  };
+    {
+      name: 'vite-plugin-electron-react',
+      apply: 'build', // only run this plugin when building the app
+      config(c) {
+        // alter the vite config
+        c.base = './';
+        c.build.outDir = 'build/renderer';
+      },
+    },
+  ];
 }
